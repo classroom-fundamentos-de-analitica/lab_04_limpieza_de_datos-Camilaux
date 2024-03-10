751afb8ed6c8d8aaea11cd721397be2ccfe9341d
Camilaux <mzapataar@unal.edu.co>
2024-03-10 16:08:48 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -13,8 +13,20 @@ def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    #
-    # Inserte su código aquí
-    #
+    #COLOCAR EN MINUSCULA TODO
+    df['sexo'] = df['sexo'].str.lower()
+    df['tipo_de_emprendimiento'] = df["tipo_de_emprendimiento"].str.lower()
+    df['idea_negocio'] = df["idea_negocio"].str.lower()
+    df['línea_credito'] = df['línea_credito'].str.lower()
+
+    #cambiar fecha
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'],format="mixed",dayfirst=True)
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].dt.strftime('%d/%m/%Y')
+
+    # Eliminar los signos de dólar y las comas, y convertir a entero
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace('$', '').str.replace(',', '').astype(float).astype(int)
+
 
     return df
+
+print(clean_data())
